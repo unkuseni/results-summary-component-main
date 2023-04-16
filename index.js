783599fb5f702063da550fd644151680b0edd082
unkuseni <amudalatadeoye@gmail.com>
2023-04-16 21:35:00 +0100

    still have to style the list

--- index.js
+++ index.js
@@ -21,18 +21,21 @@ const data = [
     "icon": "./assets/images/icon-visual.svg"
   }
 ];
-const myList = document.querySelector("#list");
+const myList = document.querySelector("#data");
+const ul = document.createElement('ul');
 data.forEach(item => {
-  const listItem = document.createElement('li');
-  const img = document.createElement('img');
-  img.setAttribute('src', item.icon);
-  img.setAttribute('alt', item.category);
-  const sp = document.createElement('span');
-  sp.textContent = `${item.score}/100`;
-  const para = document.createElement('p');
-  para.textContent = `${item.category}`;
-  listItem.appendChild(img);
-  listItem.appendChild(para);
-listItem.appendChild(sp);
-  myList.appendChild(listItem);
-})
+  const li = document.createElement('li');
+ const title = document.createElement('div')
+  const icon = document.createElement('img');
+  icon.src = item.icon;
+  title.appendChild(icon);
+  const titlePara = document.createElement('p');
+  titlePara.textContent = item.category;
+  title.appendChild(titlePara);
+  li.appendChild(title);
+  const p = document.createElement('p');
+  p.innerHTML = `<span>${item.score}</span> / 100`;
+  li.appendChild(p);
+  ul.appendChild(li);
+});
+myList.appendChild(ul);
